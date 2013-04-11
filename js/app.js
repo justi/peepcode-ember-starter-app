6916bf878916eb2d0a7eb3c1f2af82d6bd5a0181
justi <justyna@galdomedia.pl>
2013-04-11 16:57:25 +0200

    add reditect index->tables

--- js/app.js
+++ js/app.js
@@ -13,6 +13,11 @@ App.ApplicationRoute = Ember.Route.extend({
   }
 });
 
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('tables');
+  }
+})
 
 App.TablesRoute = Ember.Route.extend({
     model: function() {
